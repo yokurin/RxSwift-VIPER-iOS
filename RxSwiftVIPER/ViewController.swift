1d3def7412714ea732dd84f5dd6c358740465810
林　翼 <tsubasa_hayashi@r.recruit.co.jp>
2018-11-20 13:50:20 +0900

    Initial Commit

---
+++ RxSwiftVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftVIPER
+//
+//  Created by 林　翼 on 2018/11/20.
+//  Copyright © 2018年 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RxSwiftVIPERTests/RxSwiftVIPERTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxSwiftVIPERTests.swift
+//  RxSwiftVIPERTests
+//
+//  Created by 林　翼 on 2018/11/20.
+//  Copyright © 2018年 Tsubasa Hayashi. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwiftVIPER
+
+class RxSwiftVIPERTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
